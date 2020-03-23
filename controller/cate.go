126d626f2acf4a4de0e94134c7b9e95f7a81d222
Wuvist <wuvist@gmail.com>
2020-03-23 20:21:11 +0800

    Implement category controller

---
+++ controller/cate.go
@@ -0,0 +1,55 @@
+package controller
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/Wuvist/decho/models"
+	"github.com/Wuvist/decho/tpl"
+	"github.com/Wuvist/decho/tpl/skins"
+	"github.com/labstack/echo/v4"
+	"github.com/volatiletech/sqlboiler/queries/qm"
+)
+
+// CateController handles blog category requests
+type CateController struct {
+	bloggers models.BloggerQuery
+	cates    models.UserdefinecategoryQuery
+}
+
+// NewCateController return CateController bind with echo engine
+func NewCateController(e *echo.Echo) (*CateController, error) {
+	cate := &CateController{}
+	e.GET("/cate.go", cate.show)
+
+	return cate, nil
+}
+
+func (ctrl *CateController) show(c echo.Context) error {
+	blogerUsername := strings.ToLower(c.QueryParam("blogger"))
+	cateID := c.QueryParam("cate_id")
+	cateData, _ := ctrl.cates(qm.Where("`index` = ?", cateID)).OneG()
+	if cateData == nil {
+		return c.String(http.StatusNotFound, "找不到网志分类")
+	}
+
+	bloggerData, _ := ctrl.bloggers(qm.Where("`index` = ?", cateData.Blogger)).OneG()
+	if bloggerData == nil {
+		return c.String(http.StatusNotFound, "找不到博客")
+	}
+
+	blogger := tpl.NewBloggerFromDb(bloggerData)
+	if strings.ToLower(blogger.Username) != blogerUsername {
+		return c.String(http.StatusNotFound, "找不到分类")
+	}
+
+	cate := &tpl.Cate{}
+	cate.CateID = cateData.Index
+	cate.CateName = cateData.Cate
+
+	blogs := tpl.GetBlogSummariesFromCate(cate.CateID)
+
+	page := skins.Skin5_UserCate(blogger, cate, blogs)
+
+	return c.HTML(http.StatusOK, page)
+}
--- models/ext.go
+++ models/ext.go
@@ -4,3 +4,6 @@ import "github.com/volatiletech/sqlboiler/queries/qm"
 
 // BloggerQuery defines a func to query blogger table
 type BloggerQuery func(mods ...qm.QueryMod) bloggerQuery
+
+// UserdefinecategoryQuery defines a func to query blogger table
+type UserdefinecategoryQuery func(mods ...qm.QueryMod) userdefinecategoryQuery
