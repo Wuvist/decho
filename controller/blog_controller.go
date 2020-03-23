bdf5f520a2d9668c4b62989e13370cc66e84a026
Wuvist <wuvist@gmail.com>
2020-03-23 20:38:35 +0800

    Implement blog controller

--- controller/blog_controller.go
+++ controller/blog_controller.go
@@ -2,25 +2,76 @@ package controller
 
 import (
 	"net/http"
+	"strings"
 
+	"github.com/Wuvist/decho/models"
+	"github.com/Wuvist/decho/tpl"
+	"github.com/Wuvist/decho/tpl/skins"
 	"github.com/labstack/echo/v4"
+	"github.com/volatiletech/sqlboiler/queries/qm"
 )
 
 // BlogController defines controller for blog module
 type BlogController struct {
-	msg string
+	bloggers models.BloggerQuery
+	articles models.ArticlesQuery
 }
 
-func (controller *BlogController) hello(c echo.Context) error {
-	return c.String(http.StatusOK, controller.msg)
+func (ctrl *BlogController) show(c echo.Context) error {
+	blogerUsername := strings.ToLower(c.QueryParam("blogger"))
+	blogID := c.QueryParam("article_id")
+	blogData, err := ctrl.articles(qm.Where("`index` = ?", blogID)).OneG()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	bloggerData, _ := ctrl.bloggers(qm.Where("`index` = ?", blogData.Blogger)).OneG()
+	if bloggerData == nil {
+		return c.String(http.StatusNotFound, "找不到博客")
+	}
+
+	blogger := tpl.NewBloggerFromDb(bloggerData)
+	if strings.ToLower(blogger.Username) != blogerUsername {
+		return c.String(http.StatusNotFound, "找不到网志")
+	}
+
+	blog := tpl.NewBlogFromDb(blogData)
+	comments := tpl.GetBlogComments(blogData.Index)
+
+	page := skins.Skin5_comment(blogger, blog, comments)
+
+	return c.HTML(http.StatusOK, page)
+}
+
+func (ctrl *BlogController) showBlogger(c echo.Context) error {
+	blogerUsername := c.QueryParam("blogger")
+
+	bloggerData, err := ctrl.bloggers(qm.Where("id = ?", blogerUsername)).OneG()
+	if err != nil {
+		println(err.Error())
+	}
+	if bloggerData == nil {
+		return c.String(http.StatusNotFound, "找不到博客")
+	}
+
+	blogger := tpl.NewBloggerFromDb(bloggerData)
+
+	blogs := tpl.GetBlogSummariesFromBlogger(bloggerData.Index)
+
+	page := skins.Skin5_default(blogger, blogs)
+
+	return c.HTML(http.StatusOK, page)
 }
 
 // NewBlogController return BlogController bind with echo engine
-func NewBlogController(e *echo.Echo, msg string) (*BlogController, error) {
+func NewBlogController(e *echo.Echo, bloggers models.BloggerQuery,
+	articles models.ArticlesQuery) (*BlogController, error) {
 	blog := &BlogController{
-		msg: msg,
+		bloggers,
+		articles,
 	}
-	e.GET("/blog/hello", blog.hello)
+	e.GET("/blog.go", blog.show)
+	e.GET("/blogger.go", blog.showBlogger)
 
 	return blog, nil
 }
--- models/ext.go
+++ models/ext.go
@@ -5,5 +5,8 @@ import "github.com/volatiletech/sqlboiler/queries/qm"
 // BloggerQuery defines a func to query blogger table
 type BloggerQuery func(mods ...qm.QueryMod) bloggerQuery
 
-// UserdefinecategoryQuery defines a func to query blogger table
+// UserdefinecategoryQuery defines a func to query userdefinecategory table
 type UserdefinecategoryQuery func(mods ...qm.QueryMod) userdefinecategoryQuery
+
+// ArticlesQuery defines a func to query article table
+type ArticlesQuery func(mods ...qm.QueryMod) articleQuery
